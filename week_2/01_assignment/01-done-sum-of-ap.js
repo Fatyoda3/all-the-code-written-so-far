3b5e83dcd0da274165882126d32c26d707e9ac0b
Fatyoda3 <shivangsingh654@gmail.com>
2025-11-08 11:13:21 +0530

    `just made some more changes Ive`

--- week_2/01_assignment/01-done-sum-of-ap.js
+++ week_2/01_assignment/01-done-sum-of-ap.js
@@ -1,27 +1,39 @@
-function apSum(firstTerm, difference, numberOfTerms) {
+function apSum(firstTerm, difference, totalTerms) {
   let sum = 0;
   let sumOfDifference = 0;
-  for (let term = 0; term < numberOfTerms; term++) {
-    sum = sum + firstTerm + sumOfDifference;
+  for (let term = 0; term < totalTerms; term++) {
+    sum += firstTerm + sumOfDifference;
     sumOfDifference = sumOfDifference + difference;
   }
   return sum;
 }
 
-function testApSum(firstTerm, difference, numberOfTerms, expectedValue) {
-  const valueWeGot = apSum(firstTerm, difference, numberOfTerms)
-  const isWorking = valueWeGot === expectedValue ? '✅' : '❌';
 
-  const message = isWorking + " value we expected " + expectedValue + " and we got " + valueWeGot;
+const color = (text, code) => `\x1B[38;5;${code}m${text}\x1B[0m`;
+const bold = text => `\x1B[1m${text}\x1B[0m`;
+const fmtMsg = (input, output, expected, purpose = '') => {
+  const PrintError = output !== expected;
+  const symbol = PrintError ? "❌ " : "✅ ";
+  const message = [symbol, bold(purpose.toUpperCase())];
+
+  if (PrintError) {
+    const inpFrag = `INP -> ${color(input, 222)}`;
+    const expFrag = `EXP -> ${color(expected, 45)}`;
+    const outFrag = `OUT -> ${color(output, 196)}`;
+    message.push(`\n${inpFrag} \n${expFrag} \n${outFrag}\n`);
+  }
+
+  return color(message.join(""), 155);
+}
+const tester = (fnToTest, input, expected, intent) => {
+  const result = fnToTest(...input);
+  const message = fmtMsg(input, result, expected, intent);
   console.log(message);
 }
-
-function testAll() {
-
-  testApSum(1, 1, 10, 55);
-  testApSum(1, 1, 100, 5050);
-  testApSum(4, 1, 10, 85);
-  testApSum(1, 1, 10, 55);
+function testAll(fn) {
+  tester(fn, [1, 1, 10], 55, 'first 10 natural sum');
+  tester(fn, [1, 1, 100], 5050, '100 natural sum');
+  tester(fn, [4, 1, 10], 85, 'a=4, n=10');
 }
 
-testAll();
+testAll(apSum);
