b9035bae4fd8e0a4454701ebd4167efeac17918e
Fatyoda3 <shivangsingh654@gmail.com>
2025-11-08 11:49:48 +0530

    `i'm stupid`

--- week_2/01_assignment/04-done-even_print.js
+++ week_2/01_assignment/04-done-even_print.js
@@ -1,38 +1,50 @@
 function printEvenSeries(start, end) {
   let actualStart = start % 2 ? start + 1 : start;
-  let actualEnd = end;
+  let actualEnd = end;// bug is if end is like 1 then the code will fail all together
 
   if (start > end) {
     actualStart = end;
     actualEnd = start;
   }
 
-  let evenNumbers = '';
+  const evenNumbers = [];
 
-  for (let term = actualStart; term <= actualEnd; term += 2)
-    evenNumbers = evenNumbers + term + ' ';
-  return evenNumbers;
+  for (let term = actualStart; term <= actualEnd; term += 2) {
+    evenNumbers.push(term);
+  }
+  return evenNumbers.join(' ');
 }
 
-function testEvenSeries(start, end, expectedValue) {
-  const valueWeGot = printEvenSeries(start, end);
+const color = (text, code) => `\x1B[38;5;${code}m${text}\x1B[0m`;
+const bold = text => `\x1B[1m${text}\x1B[0m`;
+const fmtMsg = (input, output, expected, purpose = '') => {
+  const PrintError = output !== expected;
+  const symbol = PrintError ? "❌ " : "✅ ";
+  const message = [symbol, bold(purpose.toUpperCase())];
 
-  const isWorking =
-    valueWeGot === expectedValue ? '✅' : '❌';
-  const message = "value we expected " +
-    expectedValue + " and we got " +
-    valueWeGot + ' ' + isWorking;
+  if (PrintError) {
+    const inpFrag = `INP -> ${color(input, 222)}`;
+    const expFrag = `EXP -> ${color(expected, 45)}`;
+    const outFrag = `OUT -> ${color(output, 196)}`;
+    message.push(`\n${inpFrag} \n${expFrag} \n${outFrag}\n`);
+  }
 
+  return color(message.join(""), 155);
+}
+const tester = (fnToTest, input, expected, intent) => {
+  const result = fnToTest(...input);
+  const message = fmtMsg(input, result, expected, intent);
   console.log(message);
+}
+const testAll = (fn) => {
+  // tester(fn, [1, 10], '2 4 6 8 10 ');
+  // tester(fn, [1, 10], '2 4 6 8 10 ');
+  // tester(fn, [3, 10], '4 6 8 10 ');
+  // tester(fn, [-4, -12], '-12 -10 -8 -6 -4 ');
+  // tester(fn, [-4, -2], '-4 -2 ');
+  tester(fn, [10, 3], '4 6 8 10');//failed 
 
 }
 
-function runTest() {
-  testEvenSeries(1, 10, '2 4 6 8 10 ');
-  testEvenSeries(1, 10, '2 4 6 8 10 ');
-  testEvenSeries(3, 10, '4 6 8 10 ');
-  testEvenSeries(-4, -12, '-12 -10 -8 -6 -4 ');
-  testEvenSeries(-4, -2, '-4 -2 ');
-}
+testAll(printEvenSeries);
 
-runTest();
