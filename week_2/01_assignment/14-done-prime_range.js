93ce3a72126fc02c4b80ea2f4130ce2df7db926a
Fatyoda3 <shivangsingh654@gmail.com>
2025-11-08 13:24:16 +0530

    ``

--- week_2/01_assignment/14-done-prime_range.js
+++ week_2/01_assignment/14-done-prime_range.js
@@ -1,5 +1,4 @@
 const isDivisible = (dividend, divisor) => dividend % divisor === 0;
-
 const isPrime = (assumedPrime) => {
   if (assumedPrime < 2) {
     return false;
@@ -8,7 +7,6 @@ const isPrime = (assumedPrime) => {
   const root = Math.sqrt(assumedPrime);
 
   for (let divisor = 2; divisor <= root; divisor++) {
-
     if (isDivisible(assumedPrime, divisor)) {
       return false;
     }
@@ -17,22 +15,23 @@ const isPrime = (assumedPrime) => {
   return true;
 }
 
-function primeRange(start, end) {
-
+const primesInRange = (start, end) => {
   const primes = [];
 
-  for (let term = start; term <= end; term++) {
-    if (isPrime(term) && term > 1) {
+  for (let term = Math.max(start, 2); term <= end; term++) {
+
+    if (isPrime(term)) {
       primes.push(term);
     }
   }
+
   return primes.join(' ');
 }
 
 
 
 function testPrimeRange(start, end, expected) {
-  const output = primeRange(start, end);
+  const output = primesInRange(start, end);
   const works = output === expected
   const exp = works ? '✅' : '❌';
   const message = works ? 'it works' : 'it fails';
@@ -46,6 +45,11 @@ function testAll() {
   testPrimeRange(2, 25, '2 3 5 7 11 13 17 19 23');
   testPrimeRange(0, 5, '2 3 5');
   testPrimeRange(101, 110, '101 103 107 109');
+  testPrimeRange(0, 0, '');
+  testPrimeRange(0, 1, '');
+  testPrimeRange(0, 2, '2');
+  testPrimeRange(1, 2, '2');
+  testPrimeRange(7, 9, '7');
 }
 
 testAll();
